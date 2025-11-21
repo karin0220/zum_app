f0ce2e2a8a6bc5d2909ebf5d4fce0559edf42a41
karin0220 <karin0220@gmail.com>
2025-11-21 15:09:14 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 없어도 에러 안 나게 비워두거나 파일 안 만들어도 됨
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
